7cbcb82a2d8bcaa0bf5f03a95630316d92789973
justin.farrell <justin.farrell@lightcast.io>
2023-04-25 23:02:49 -0400

    Added ESLint and Prettier to the project and ran them on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import { Typography } from "@mui/material";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Typography } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Typography variant="h3">Call to Power Mod Manager</Typography>,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>

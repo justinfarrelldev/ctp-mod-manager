18534be14eea893a6291a4c3be55a4daca2761a1
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-06-30 01:10:48 -0400

    Filled out theme settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,17 @@
+import { MenuItem, Select, Typography } from '@mui/material';
 import React, { FC } from 'react';
 
 export const Settings: FC = (): React.ReactElement => {
-  return <>Temp settings</>;
+  return (
+    <>
+      <Typography>General Settings</Typography>
+      <Select label="Theme" defaultValue="light">
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="cafe">Cafe</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="tokyo-night">Tokyo Night</MenuItem>
+        <MenuItem value="amoled">Amoled</MenuItem>
+      </Select>
+    </>
+  );
 };

61bfde0217840595ae24fec46773105dcbb9e6e5
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-07-07 00:35:29 -0400

    Moved ModInterface into components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { App } from './App';
-import { ModInterface } from './ModInterface';
+import { ModInterface } from './components/ModInterface';
 
 const router = createBrowserRouter([
   {

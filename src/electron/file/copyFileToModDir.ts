fbdf61dddcaab54d41afac707b7bd494b06255d3
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-07-21 23:32:41 -0400

    Unzipping to mod directory

--- src/electron/file/copyFileToModDir.ts
+++ src/electron/file/copyFileToModDir.ts
@@ -1,7 +1,24 @@
 import { app } from 'electron';
 import { DEFAULT_MOD_DIR, DEFAULT_MOD_FOLDER_NAME } from '../constants';
 import fs from 'fs';
+import AdmZip from 'adm-zip';
 
+const unzipInModDir = (zipFullPath: string): void => {
+  try {
+    const zip = new AdmZip(zipFullPath);
+    zip.extractAllTo(DEFAULT_MOD_DIR);
+  } catch (err) {
+    console.error(`Failed to unzip: ${err}`);
+  }
+};
+
+// Copy file to mod dir should:
+// - Extract the files of the zip to the mods folder (preserve original)
+// - Determine if that is a zip (unzip if it is, and repeat until there is not a zip)
+// - Determine if this is a scenario file or a full-on mod (does it have scen0000? Is there a ctp2_data or ctp2_program?)
+// - If scenario, simply say this does not support scenarios at the moment and will in a later release
+// - If it has a ctp2_data / ctp2_program folder, set that as the top-level and zip it (later, I should really add the readme to this level too...)
+// - Use that zip in the mod folder (so we can have comparisons of each file easily)
 export const copyFileToModDir = async (fileDir: string) => {
   const split = fileDir.split('\\');
   const fileName = split[split.length - 1];
@@ -21,6 +38,7 @@ export const copyFileToModDir = async (fileDir: string) => {
   }
 
   const destination = `${DEFAULT_MOD_DIR}\\${fileName}`;
+  unzipInModDir(fileDir);
   fs.copyFile(fileDir, destination, (err) => {
     if (err) throw err;
     // eslint-disable-next-line no-console

bcc5237df28145d0548c2994644970613126dfc4
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-10-26 23:22:34 -0400

    createBrowserRouter -> createHashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { App } from './App';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,

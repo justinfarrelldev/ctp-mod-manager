df607f41608321665f19d87394fb8e5be2cc8395
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-05-09 19:32:33 -0400

    Made Prettier work again and replaced top text and grid with Tailwind / HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Typography, Box, Grid, CircularProgress as Loader, Tooltip, Button } from '@mui/material';
+import {
+    Typography,
+    Box,
+    Grid,
+    CircularProgress as Loader,
+    Tooltip,
+    Button,
+} from '@mui/material';
 import { Settings } from '@mui/icons-material';
 import { Settings as SettingsMenu } from './components/Settings';
 import { Modal } from './components/Modal';
@@ -8,232 +15,280 @@ import { InstallDirTable } from './components/InstallDirTable';
 import { IZipEntry } from 'adm-zip';
 import { ErrorModal } from './components/ErrorModal';
 import { ModifyInstallView } from './components/ModifyInstallView';
+import { SettingsIcon } from './components/icons/settings';
 
 export type ElectronWindow = Window &
-  typeof globalThis & {
-    api: {
-      getCtp2InstallDir: () => Promise<
-        [
-          {
-            directory: string;
-            installationType: 'steam' | 'gog';
-            os: string;
-          }
-        ]
-      >;
-      openInstallDir: (ipcCommand: string, dir: string) => void;
-      openModsDir: (ipcCommand: string) => void;
-      copyFileToModDir: (ipcCommand: string, fileDir: string) => Promise<void>;
-      viewFileDirsInZip: (ipcCommand: string, zipFilePath: string) => Promise<string[]>;
-      goToRoute: (ipcCommand: string, route: string) => void;
-      loadMods: () => Promise<string[]>;
-      getModsDir: (ipcCommand: string) => Promise<string>;
-      selectFolder: (ipcCommand: string) => Promise<string>;
-      isValidInstall: (ipcCommand: string, dir: string) => Promise<boolean>;
-      addToInstallDirs: (ipcCommand: string, dir: string) => Promise<void>;
-      getInstallDirs: (ipcCommand: 'file:getInstallDirs') => Promise<string[]>;
-      runGame: (ipcCommand: 'file:runGame', exeDir: string) => void;
-      removeFromInstallDirs: (
-        ipcCommand: 'file:removeFromInstallDirs',
-        dir: string
-      ) => Promise<void>;
-      makeBackup: (ipcCommand: 'file:makeBackup', installDir: string) => Promise<void>;
-      applyModsToInstall: (
-        ipcCommand: 'file:applyModsToInstall',
-        installDir: string,
-        mods: string[]
-      ) => Promise<void>;
+    typeof globalThis & {
+        api: {
+            getCtp2InstallDir: () => Promise<
+                [
+                    {
+                        directory: string;
+                        installationType: 'steam' | 'gog';
+                        os: string;
+                    },
+                ]
+            >;
+            openInstallDir: (ipcCommand: string, dir: string) => void;
+            openModsDir: (ipcCommand: string) => void;
+            copyFileToModDir: (
+                ipcCommand: string,
+                fileDir: string
+            ) => Promise<void>;
+            viewFileDirsInZip: (
+                ipcCommand: string,
+                zipFilePath: string
+            ) => Promise<string[]>;
+            goToRoute: (ipcCommand: string, route: string) => void;
+            loadMods: () => Promise<string[]>;
+            getModsDir: (ipcCommand: string) => Promise<string>;
+            selectFolder: (ipcCommand: string) => Promise<string>;
+            isValidInstall: (
+                ipcCommand: string,
+                dir: string
+            ) => Promise<boolean>;
+            addToInstallDirs: (
+                ipcCommand: string,
+                dir: string
+            ) => Promise<void>;
+            getInstallDirs: (
+                ipcCommand: 'file:getInstallDirs'
+            ) => Promise<string[]>;
+            runGame: (ipcCommand: 'file:runGame', exeDir: string) => void;
+            removeFromInstallDirs: (
+                ipcCommand: 'file:removeFromInstallDirs',
+                dir: string
+            ) => Promise<void>;
+            makeBackup: (
+                ipcCommand: 'file:makeBackup',
+                installDir: string
+            ) => Promise<void>;
+            applyModsToInstall: (
+                ipcCommand: 'file:applyModsToInstall',
+                installDir: string,
+                mods: string[]
+            ) => Promise<void>;
+        };
     };
-  };
 
 export type InstallDirectory = {
-  os: string;
-  directory: string;
-  installationType: 'steam' | 'gog';
+    os: string;
+    directory: string;
+    installationType: 'steam' | 'gog';
 };
 
 export const App: FC = (): React.ReactElement => {
-  const [loadingDirs, setLoadingDirs] = useState<boolean>();
-  const [installDirModalOpen, setInstallDirModalOpen] = useState<boolean>(false);
-  const [installDirs, setInstallDirs] = useState<InstallDirectory[]>([]);
-  const [dirBeingModified, setDirBeingModified] = useState<string>('');
-  const [settingsOpen, setSettingsOpen] = useState<boolean>();
-  const [modNamesAdded, setModNamesAdded] = useState<string[] | undefined>(undefined);
-  const [modNamesQueued, setModNamesQueued] = useState<string[]>([]);
-  const [error, setError] = useState<string>();
-
-  const loadMods = async (): Promise<void> => {
-    try {
-      const loadedMods = await (window as ElectronWindow).api.loadMods();
-      setModNamesAdded(loadedMods);
-    } catch (err) {
-      console.error(`An error occurred within App while setting mod names: ${err}`);
-      setError(`An error occurred while attempting to load mods: ${err}.`);
-    }
-  };
-
-  const loadInstallDirs = async (): Promise<void> => {
-    setLoadingDirs(true);
-    const dirsFromFile = await (window as ElectronWindow).api.getInstallDirs('file:getInstallDirs');
-
-    setInstallDirs(
-      dirsFromFile.map((dir) => ({ directory: dir, installationType: 'steam', os: 'win32' }))
+    const [loadingDirs, setLoadingDirs] = useState<boolean>();
+    const [installDirModalOpen, setInstallDirModalOpen] =
+        useState<boolean>(false);
+    const [installDirs, setInstallDirs] = useState<InstallDirectory[]>([]);
+    const [dirBeingModified, setDirBeingModified] = useState<string>('');
+    const [settingsOpen, setSettingsOpen] = useState<boolean>();
+    const [modNamesAdded, setModNamesAdded] = useState<string[] | undefined>(
+        undefined
     );
-    setLoadingDirs(false);
-    if (dirsFromFile.length === 0) {
-      setInstallDirModalOpen(true);
-    }
-  };
+    const [modNamesQueued, setModNamesQueued] = useState<string[]>([]);
+    const [error, setError] = useState<string>();
 
-  const findInstallDirs = async (): Promise<void> => {
-    setLoadingDirs(true);
-    const dirs: InstallDirectory[] = await (window as ElectronWindow).api.getCtp2InstallDir();
+    const loadMods = async (): Promise<void> => {
+        try {
+            const loadedMods = await (window as ElectronWindow).api.loadMods();
+            setModNamesAdded(loadedMods);
+        } catch (err) {
+            console.error(
+                `An error occurred within App while setting mod names: ${err}`
+            );
+            setError(
+                `An error occurred while attempting to load mods: ${err}.`
+            );
+        }
+    };
 
-    for (const dir of dirs) {
-      // For the sake of speed, I am disabling this
-      // eslint-disable-next-line no-await-in-loop
-      await (window as ElectronWindow).api.addToInstallDirs('file:addToInstallDirs', dir.directory);
-    }
+    const loadInstallDirs = async (): Promise<void> => {
+        setLoadingDirs(true);
+        const dirsFromFile = await (
+            window as ElectronWindow
+        ).api.getInstallDirs('file:getInstallDirs');
 
-    setInstallDirs([...installDirs, ...dirs]);
+        setInstallDirs(
+            dirsFromFile.map((dir) => ({
+                directory: dir,
+                installationType: 'steam',
+                os: 'win32',
+            }))
+        );
+        setLoadingDirs(false);
+        if (dirsFromFile.length === 0) {
+            setInstallDirModalOpen(true);
+        }
+    };
 
-    await loadMods();
+    const findInstallDirs = async (): Promise<void> => {
+        setLoadingDirs(true);
+        const dirs: InstallDirectory[] = await (
+            window as ElectronWindow
+        ).api.getCtp2InstallDir();
 
-    setLoadingDirs(false);
-  };
+        for (const dir of dirs) {
+            // For the sake of speed, I am disabling this
+            // eslint-disable-next-line no-await-in-loop
+            await (window as ElectronWindow).api.addToInstallDirs(
+                'file:addToInstallDirs',
+                dir.directory
+            );
+        }
 
-  const handleInstallDirModalClose = (): void => {
-    setInstallDirModalOpen(false);
-  };
+        setInstallDirs([...installDirs, ...dirs]);
 
-  const handleFileSelected = async (e: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
-    const files = Array.from(e.target.files);
-    await (window as ElectronWindow).api.copyFileToModDir(
-      'file:copy',
-      (files[0] as File & { path: string }).path
-    );
+        await loadMods();
 
-    await loadMods();
-  };
+        setLoadingDirs(false);
+    };
 
-  const openModsDir = (): void => {
-    console.log('opening mods dir');
-    (window as ElectronWindow).api.openModsDir('file:openModsDir');
-  };
+    const handleInstallDirModalClose = (): void => {
+        setInstallDirModalOpen(false);
+    };
 
-  const viewFileDirsInZip = async (zipFilePath: string): Promise<string[]> => {
-    const contents = await (window as ElectronWindow).api.viewFileDirsInZip(
-      'file:viewFileDirsInZip',
-      zipFilePath
-    );
+    const handleFileSelected = async (
+        e: React.ChangeEvent<HTMLInputElement>
+    ): Promise<void> => {
+        const files = Array.from(e.target.files);
+        await (window as ElectronWindow).api.copyFileToModDir(
+            'file:copy',
+            (files[0] as File & { path: string }).path
+        );
 
-    console.log('contents: ', contents);
-    return contents;
-  };
+        await loadMods();
+    };
 
-  const getModsDir = async (): Promise<string> => {
-    return await (window as ElectronWindow).api.getModsDir('file:getModsDir');
-  };
+    const openModsDir = (): void => {
+        console.log('opening mods dir');
+        (window as ElectronWindow).api.openModsDir('file:openModsDir');
+    };
 
-  useEffect(() => {
-    loadMods();
-    loadInstallDirs();
-  }, []);
+    const viewFileDirsInZip = async (
+        zipFilePath: string
+    ): Promise<string[]> => {
+        const contents = await (window as ElectronWindow).api.viewFileDirsInZip(
+            'file:viewFileDirsInZip',
+            zipFilePath
+        );
 
-  return (
-    <>
-      <Grid container>
-        <Grid item xs={6}>
-          <Box display="flex">
-            <Typography variant="h4">Call to Power 2 Installations</Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={6} textAlign="right">
-          <Settings onClick={() => setSettingsOpen(true)} />
-        </Grid>
-      </Grid>
+        console.log('contents: ', contents);
+        return contents;
+    };
 
-      <h1 className="text-orange-200">Orange!</h1>
+    const getModsDir = async (): Promise<string> => {
+        return await (window as ElectronWindow).api.getModsDir(
+            'file:getModsDir'
+        );
+    };
 
-      <button className="btn btn-primary">This is a test</button>
+    useEffect(() => {
+        loadMods();
+        loadInstallDirs();
+    }, []);
 
-      {error && (
-        <ErrorModal
-          open={error.length > 0}
-          errorMessage={error}
-          onClose={() => {
-            setError('');
-          }}
-        />
-      )}
-      {loadingDirs && <Loader />}
-      {settingsOpen && (
-        <Modal width="50%" open={settingsOpen} onClose={() => setSettingsOpen(false)}>
-          <SettingsMenu />
-        </Modal>
-      )}
+    return (
+        <>
+            <p className="top-2 text-2xl">Call to Power 2 Installations</p>
+            <div className="absolute right-0 top-2 h-16 w-16">
+                <button onClick={() => setSettingsOpen(true)}>
+                    <SettingsIcon />
+                </button>
+            </div>
 
-      <InstallDirTable
-        installDirs={installDirs}
-        onClickModify={(dir) => setDirBeingModified(dir)}
-        onAddedInstallDirectory={() => loadInstallDirs()}
-      />
-      <Modal
-        open={dirBeingModified !== ''}
-        onClose={() => {
-          setDirBeingModified('');
-        }}
-        width="100%"
-        height="100%"
-      >
-        <ModifyInstallView
-          onBackClicked={() => setDirBeingModified('')}
-          dirBeingModified={dirBeingModified}
-          onModSelected={handleFileSelected}
-          onQueueMod={async (modName: string) => {
-            setModNamesQueued([...modNamesQueued, modName]);
-            setModNamesAdded(modNamesAdded.filter((value) => value !== modName));
-            viewFileDirsInZip(`${await getModsDir()}\\${modName}`); // FIXME 100% temporary
-          }}
-          onDequeueMod={async (modName: string) => {
-            setModNamesQueued(modNamesQueued.filter((value) => value !== modName));
-            setModNamesAdded([...modNamesAdded, modName]);
-          }}
-          addedMods={modNamesAdded}
-          queuedMods={modNamesQueued}
-          onOpenModsDir={() => openModsDir()}
-        />
-      </Modal>
-      <Modal width="50%" open={installDirModalOpen} onClose={handleInstallDirModalClose}>
-        <Box>
-          <Grid container rowSpacing="1rem">
-            <Grid item xs={12}>
-              <Typography textAlign="center">{AUTO_DETECT_INSTALL_TEXT}</Typography>
-            </Grid>
-            <Grid item xs={6} textAlign="center">
-              <button
-                onClick={() => {
-                  findInstallDirs();
-                  handleInstallDirModalClose();
+            {error && (
+                <ErrorModal
+                    open={error.length > 0}
+                    errorMessage={error}
+                    onClose={() => {
+                        setError('');
+                    }}
+                />
+            )}
+            {loadingDirs && <Loader />}
+            {settingsOpen && (
+                <Modal
+                    width="50%"
+                    open={settingsOpen}
+                    onClose={() => setSettingsOpen(false)}
+                >
+                    <SettingsMenu />
+                </Modal>
+            )}
+
+            <InstallDirTable
+                installDirs={installDirs}
+                onClickModify={(dir) => setDirBeingModified(dir)}
+                onAddedInstallDirectory={() => loadInstallDirs()}
+            />
+            <Modal
+                open={dirBeingModified !== ''}
+                onClose={() => {
+                    setDirBeingModified('');
                 }}
-                className="btn btn-primary"
-              >
-                Yes
-              </button>
-            </Grid>
-            <Grid item xs={6} textAlign="center">
-              <Button
-                onClick={() => {
-                  handleInstallDirModalClose();
-                }}
-                variant="outlined"
-              >
-                No
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Modal>
-    </>
-  );
+                width="100%"
+                height="100%"
+            >
+                <ModifyInstallView
+                    onBackClicked={() => setDirBeingModified('')}
+                    dirBeingModified={dirBeingModified}
+                    onModSelected={handleFileSelected}
+                    onQueueMod={async (modName: string) => {
+                        setModNamesQueued([...modNamesQueued, modName]);
+                        setModNamesAdded(
+                            modNamesAdded.filter((value) => value !== modName)
+                        );
+                        viewFileDirsInZip(`${await getModsDir()}\\${modName}`); // FIXME 100% temporary
+                    }}
+                    onDequeueMod={async (modName: string) => {
+                        setModNamesQueued(
+                            modNamesQueued.filter((value) => value !== modName)
+                        );
+                        setModNamesAdded([...modNamesAdded, modName]);
+                    }}
+                    addedMods={modNamesAdded}
+                    queuedMods={modNamesQueued}
+                    onOpenModsDir={() => openModsDir()}
+                />
+            </Modal>
+            <Modal
+                width="50%"
+                open={installDirModalOpen}
+                onClose={handleInstallDirModalClose}
+            >
+                <Box>
+                    <Grid container rowSpacing="1rem">
+                        <Grid item xs={12}>
+                            <Typography textAlign="center">
+                                {AUTO_DETECT_INSTALL_TEXT}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="center">
+                            <button
+                                onClick={() => {
+                                    findInstallDirs();
+                                    handleInstallDirModalClose();
+                                }}
+                                className="btn btn-primary"
+                            >
+                                Yes
+                            </button>
+                        </Grid>
+                        <Grid item xs={6} textAlign="center">
+                            <Button
+                                onClick={() => {
+                                    handleInstallDirModalClose();
+                                }}
+                                variant="outlined"
+                            >
+                                No
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Modal>
+        </>
+    );
 };

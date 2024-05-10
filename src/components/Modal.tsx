9dacc0366f34e5816ca4759dd246f79f37968d17
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-05-09 20:38:51 -0400

    Cleaned up Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,4 @@
 import React, { FC } from 'react';
-import {
-    Box,
-    Modal as MuiModal,
-    ModalProps as MuiModalProps,
-} from '@mui/material';
-
-const style = {
-    position: 'absolute' as const,
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
 export interface ModalProps {
     width: string;

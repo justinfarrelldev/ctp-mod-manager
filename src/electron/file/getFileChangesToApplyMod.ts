8064faffb43647a33f55bd14e36d24438a903c94
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-05-11 16:34:18 -0400

    Made all calls async but they still freeze the UI

--- src/electron/file/getFileChangesToApplyMod.ts
+++ src/electron/file/getFileChangesToApplyMod.ts
@@ -1,7 +1,8 @@
 import { DEFAULT_MOD_DIR } from '../constants';
-import fs from 'node:fs';
+import fs, { ObjectEncodingOptions } from 'node:fs';
 import path from 'node:path';
 import * as diff from 'diff';
+import pLimit from 'p-limit';
 
 // Define the type for the nested object structure
 type DirectoryContents = {
@@ -21,57 +22,136 @@ type FileChange = {
     fileName: string;
     lineChangeGroups: LineChangeGroup[];
 };
+// Limit the number of concurrent file reads
+const limit = pLimit(50);
 
-const processDirectory = (
+const readdirPromisified = async (
+    path: fs.PathLike,
+    options: ObjectEncodingOptions & {
+        withFileTypes: true;
+    }
+): Promise<fs.Dirent[]> => {
+    return new Promise((resolve) => {
+        fs.readdir(path, options, (err, files) => {
+            if (err)
+                console.error(
+                    `An error occurred while reading a directory: ${err}`
+                );
+
+            resolve(files);
+        });
+    });
+};
+
+const readfilePromisified = async (fullPath: string): Promise<string> => {
+    return new Promise((resolve) => {
+        fs.readFile(fullPath, 'utf-8', (err, data) => {
+            if (err)
+                console.error(`An error occurred while reading a file: ${err}`);
+
+            resolve(data);
+        });
+    });
+};
+
+// Function to read directory contents recursively
+const readDirectory = (dirPath: string): DirectoryContents => {
+    const entries = fs.readdirSync(dirPath, { withFileTypes: true });
+    const result: DirectoryContents = {};
+
+    for (const entry of entries) {
+        const fullPath = path.join(dirPath, entry.name);
+        if (entry.isDirectory()) {
+            result[entry.name] = readDirectory(fullPath);
+        } else {
+            const fileContents = fs.readFileSync(fullPath, 'utf-8');
+            result[entry.name] = fileContents;
+        }
+    }
+
+    return result;
+};
+
+// FIXME this solution is async and makes use of p-limit, but it's too slow at reading the game dir. A stutter is better than 5 mins of waiting
+// async function readDirectory(dirPath: string): Promise<DirectoryContents> {
+//     const entries = await readdirPromisified(dirPath, { withFileTypes: true });
+//     const result: DirectoryContents = {};
+
+//     await Promise.all(
+//         entries.map((entry) =>
+//             limit(async () => {
+//                 const fullPath = path.join(dirPath, entry.name);
+//                 if (entry.isDirectory()) {
+//                     result[entry.name] = await readDirectory(fullPath);
+//                 } else {
+//                     result[entry.name] = await readfilePromisified(fullPath);
+//                 }
+//             })
+//         )
+//     );
+
+//     return result;
+// }
+
+async function processDirectory(
     oldContent: DirectoryContents,
     newContent: DirectoryContents,
     prefix: string = ''
-) => {
-    const changes: FileChange[] = [];
+): Promise<FileChange[]> {
+    let changes: FileChange[] = [];
+    let promises: Promise<void>[] = [];
+
+    console.log('prefix: ', prefix);
     for (const key of Object.keys(newContent)) {
         const oldFilePath = oldContent[key];
         const newFilePath = newContent[key];
         const fullPath = prefix + key;
 
+        console.log('now processing: ', fullPath);
+
         if (
             typeof newFilePath === 'object' &&
             typeof oldFilePath === 'object'
         ) {
-            // Recursive case: both are directories
-            processDirectory(oldFilePath, newFilePath, fullPath + '/');
+            promises.push(
+                processDirectory(oldFilePath, newFilePath, fullPath + '/').then(
+                    (value) => {
+                        changes = [...changes, ...value];
+                    }
+                )
+            );
         } else if (
             typeof newFilePath === 'string' &&
             typeof oldFilePath === 'string'
         ) {
-            // Base case: both are files
-            const fileChanges = getFileChanges(
-                fullPath,
-                oldFilePath,
-                newFilePath
+            promises.push(
+                getFileChanges(fullPath, oldFilePath, newFilePath).then(
+                    (fileChanges) => {
+                        if (fileChanges.lineChangeGroups.length > 0) {
+                            changes.push(fileChanges);
+                        }
+                    }
+                )
             );
-            if (fileChanges.lineChangeGroups.length > 0) {
-                changes.push(fileChanges);
-            }
         }
     }
+    return Promise.all(promises).then(() => {
+        return changes;
+    });
+}
 
-    return changes;
-};
-
-// Function to compare two DirectoryContents objects
-const compareDirectories = (
+async function compareDirectories(
     oldDir: DirectoryContents,
     newDir: DirectoryContents
-): FileChange[] => {
+): Promise<FileChange[]> {
     return processDirectory(oldDir, newDir);
-};
+}
 
-// Function to get file changes
-const getFileChanges = (
+async function getFileChanges(
     fileName: string,
     oldContent: string,
     newContent: string
-): FileChange => {
+): Promise<FileChange> {
     const changeDiffs = diff.diffLines(oldContent, newContent);
     const lineChangeGroups: LineChangeGroup[] = [];
     let lineIndex = 1;
@@ -94,30 +174,14 @@ const getFileChanges = (
         fileName,
         lineChangeGroups,
     };
-};
+}
 
-// Function to read directory contents recursively
-const readDirectory = (dirPath: string): DirectoryContents => {
-    const entries = fs.readdirSync(dirPath, { withFileTypes: true });
-    const result: DirectoryContents = {};
-
-    for (const entry of entries) {
-        const fullPath = path.join(dirPath, entry.name);
-        if (entry.isDirectory()) {
-            result[entry.name] = readDirectory(fullPath);
-        } else {
-            const fileContents = fs.readFileSync(fullPath, 'utf-8');
-            result[entry.name] = fileContents;
-        }
-    }
-
-    return result;
-};
-
-export const getFileChangesToApplyMod = (
+export const getFileChangesToApplyMod = async (
     mod: string,
     installDir: string
 ): Promise<FileChange[]> => {
+    console.log('Running code!');
+
     let statsOfFile: fs.Stats | undefined;
     try {
         statsOfFile = fs.statSync(`${DEFAULT_MOD_DIR}\\${mod}`);
@@ -136,15 +200,17 @@ export const getFileChangesToApplyMod = (
             return;
         }
 
+        console.log('reading mod dir structure!');
         const modDirStructure = readDirectory(`${DEFAULT_MOD_DIR}\\${mod}`);
 
+        console.log('reading game dir structure!');
         const gameDirStructure = readDirectory(installDir);
 
-        const fileChanges = compareDirectories(
-            gameDirStructure,
-            modDirStructure
+        console.log('comparing dirs!');
+        compareDirectories(gameDirStructure, modDirStructure).then(
+            (fileChanges) => {
+                console.log('file changes: ', JSON.stringify(fileChanges));
+            }
         );
-
-        console.log('file changes: ', JSON.stringify(fileChanges));
     }
 };

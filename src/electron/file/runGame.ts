0aaae494d6592ff9ed1a1e30ffb6b84c6caef58b
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-05-11 01:19:38 -0400

    Changed the game launching aspect to use spawn instead of execFileSync

--- src/electron/file/runGame.ts
+++ src/electron/file/runGame.ts
@@ -1,6 +1,19 @@
-import { execFileSync } from 'child_process';
+import { spawn } from 'child_process';
 
 export const runGame = (exeDir: string): void => {
     console.log('executing exe: ', exeDir);
-    execFileSync(exeDir, []);
+
+    const game = spawn(exeDir, []);
+
+    game.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    game.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    game.on('close', (code) => {
+        console.log(`Game process exited with code ${code}`);
+    });
 };

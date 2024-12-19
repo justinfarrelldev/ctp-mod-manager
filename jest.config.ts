eae6c08c8574ab808edcb7922c8546bf49849641
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-12-18 21:31:53 -0500

    Remove Jest configuration and tests, migrate to Vitest for testing framework

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist'],
-    setupFilesAfterEnv: ['<rootDir>/src/setup-tests.ts'],
-    runner: 'groups',
-};

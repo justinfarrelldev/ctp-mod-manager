7f7bd01a025667f5836f49146e967ea76b8854ee
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-01-03 21:12:16 -0500

    Add test for overlapping changes across multiple mods and files in areFileChangesValid

--- src/electron/file/applyFileChanges.test.ts
+++ src/electron/file/applyFileChanges.test.ts
@@ -169,4 +169,88 @@ describe('areFileChangesValid', () => {
 
         expect(areFileChangesValid({ modFileChanges })).toBe(false);
     });
+
+    it('should return false for overlapping changes across multiple mods and multiple files', () => {
+        const changeGroup1: LineChangeGroup[] = [
+            {
+                startLineNumber: 1,
+                endLineNumber: 3,
+                newContent: 'new content 1',
+                changeType: 'add',
+            },
+        ];
+
+        const changeGroup2: LineChangeGroup[] = [
+            {
+                startLineNumber: 2,
+                endLineNumber: 4,
+                oldContent: 'old content 2',
+                changeType: 'remove',
+            },
+        ];
+
+        const changeGroup3: LineChangeGroup[] = [
+            {
+                startLineNumber: 5,
+                endLineNumber: 6,
+                newContent: 'new content 3',
+                changeType: 'add',
+            },
+        ];
+
+        const changeGroup4: LineChangeGroup[] = [
+            {
+                startLineNumber: 1,
+                endLineNumber: 2,
+                newContent: 'new content 4',
+                changeType: 'add',
+            },
+        ];
+
+        const fileChanges1: FileChange[] = [
+            {
+                fileName: 'file1.txt',
+                lineChangeGroups: changeGroup1,
+                isBinary: false,
+            },
+            {
+                fileName: 'file2.txt',
+                lineChangeGroups: changeGroup3,
+                isBinary: false,
+            },
+        ];
+
+        const fileChanges2: FileChange[] = [
+            {
+                fileName: 'file1.txt',
+                lineChangeGroups: changeGroup2,
+                isBinary: false,
+            },
+        ];
+
+        const fileChanges3: FileChange[] = [
+            {
+                fileName: 'file3.txt',
+                lineChangeGroups: changeGroup4,
+                isBinary: false,
+            },
+        ];
+
+        const modFileChanges = [
+            {
+                mod: 'mod1',
+                fileChanges: fileChanges1,
+            },
+            {
+                mod: 'mod2',
+                fileChanges: fileChanges2,
+            },
+            {
+                mod: 'mod3',
+                fileChanges: fileChanges3,
+            },
+        ];
+
+        expect(areFileChangesValid({ modFileChanges })).toBe(false);
+    });
 });

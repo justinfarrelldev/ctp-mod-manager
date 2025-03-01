22f3813fce246d58025ff7a5f34930e9f02aa0da
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-02-28 21:56:29 -0500

    Add test for consolidating multiple line changes into replace operations

--- src/electron/file/getFileChangesToApplyMod.test.ts
+++ src/electron/file/getFileChangesToApplyMod.test.ts
@@ -174,4 +174,127 @@ describe('consolidateLineChangeGroups', () => {
 
         expect(consolidateLineChangeGroups(input)).toEqual(expected);
     });
+
+    it('should consolidate 6 file changes (flattened) into 3 replace operations with the expected content', () => {
+        const fileChanges = [
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 2,
+                        endLineNumber: 2,
+                        changeType: 'remove',
+                        oldContent:
+                            'COLORSET_COLOR\t\t196 0 38\t\t#\tCOLOR_PLAYER0   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 2,
+                        endLineNumber: 2,
+                        changeType: 'add',
+                        newContent:
+                            'COLORSET_COLOR\t\t32 32 32\t\t#\tCOLOR_PLAYER0   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 3,
+                        endLineNumber: 3,
+                        changeType: 'remove',
+                        oldContent:
+                            'COLORSET_COLOR\t\t138 59 204\t\t#\tCOLOR_PLAYER5   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 3,
+                        endLineNumber: 3,
+                        changeType: 'add',
+                        newContent:
+                            'COLORSET_COLOR\t\t200 0 0\t\t\t#\tCOLOR_PLAYER5   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 4,
+                        endLineNumber: 4,
+                        changeType: 'remove',
+                        oldContent:
+                            'COLORSET_COLOR\t\t137 98 53\t\t#\tCOLOR_PLAYER8   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+            {
+                fileName: 'ctp2_data/default/gamedata/Colors00.txt',
+                lineChangeGroups: [
+                    {
+                        startLineNumber: 4,
+                        endLineNumber: 4,
+                        changeType: 'add',
+                        newContent:
+                            'COLORSET_COLOR\t\t255 255 64\t\t#\tCOLOR_PLAYER8   \t\t\t',
+                    },
+                ],
+                isBinary: false,
+            },
+        ];
+
+        // Flatten all line change groups into a single array
+        const allLineChangeGroups = fileChanges.flatMap(
+            (fc) => fc.lineChangeGroups as LineChangeGroup[]
+        );
+
+        const consolidated = consolidateLineChangeGroups(allLineChangeGroups);
+
+        const expected = [
+            {
+                changeType: 'replace',
+                startLineNumber: 2,
+                endLineNumber: 2,
+                oldContent:
+                    'COLORSET_COLOR\t\t196 0 38\t\t#\tCOLOR_PLAYER0   \t\t\t',
+                newContent:
+                    'COLORSET_COLOR\t\t32 32 32\t\t#\tCOLOR_PLAYER0   \t\t\t',
+            },
+            {
+                changeType: 'replace',
+                startLineNumber: 3,
+                endLineNumber: 3,
+                oldContent:
+                    'COLORSET_COLOR\t\t138 59 204\t\t#\tCOLOR_PLAYER5   \t\t\t',
+                newContent:
+                    'COLORSET_COLOR\t\t200 0 0\t\t\t#\tCOLOR_PLAYER5   \t\t\t',
+            },
+            {
+                changeType: 'replace',
+                startLineNumber: 4,
+                endLineNumber: 4,
+                oldContent:
+                    'COLORSET_COLOR\t\t137 98 53\t\t#\tCOLOR_PLAYER8   \t\t\t',
+                newContent:
+                    'COLORSET_COLOR\t\t255 255 64\t\t#\tCOLOR_PLAYER8   \t\t\t',
+            },
+        ];
+
+        // Ensure that we get 3 consolidated line change groups and all of them are "replace" types.
+        expect(consolidated).toEqual(expected);
+    });
 });

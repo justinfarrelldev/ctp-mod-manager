79fe4ed23585811fade23158bb5a01c6b7314be4
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-03-03 18:17:15 -0500

    Refactor diffDirectories to use p-limit for improved concurrency in directory comparisons

--- src/electron/file/diffDirectories.ts
+++ src/electron/file/diffDirectories.ts
@@ -22,12 +22,15 @@
     - Return the FileChange[]
 */
 
+import pLimit from 'p-limit';
 import { FileChange } from './fileChange';
 import { diffTexts } from './getFileChangesToApplyMod';
 import { isBinaryFile } from './isBinaryFile';
 import { LineChangeGroup } from './lineChangeGroup';
 import { DirectoryContents } from './readDirectory';
 import * as crypto from 'crypto';
+
+const MAX_PROMISES_ALLOWED = 100;
 
 export const countLines = (str: string): number => {
     // We rely on '\n' having char code 10
@@ -78,150 +81,155 @@ export const diffDirectories = async ({
     // Every time we see a file, we will remove it from this list
     // so that we can determine which files were deleted
     const oldDirFileNames: string[] = Object.keys(oldDir);
+    const limit = pLimit(MAX_PROMISES_ALLOWED);
 
-    const promises = Object.entries(newDir).map(async ([k, v]) => {
-        oldDirFileNames.splice(oldDirFileNames.indexOf(k), 1);
-        let fileName = k;
-        const newFileContents = v;
-        const oldFileContents = oldDir[fileName];
-        const fullPath = parentPath ? `${parentPath}/${fileName}` : fileName;
+    const promises = Object.entries(newDir).map(([k, v]) =>
+        limit(async () => {
+            oldDirFileNames.splice(oldDirFileNames.indexOf(k), 1);
+            let fileName = k;
+            const newFileContents = v;
+            const oldFileContents = oldDir[fileName];
+            const fullPath = parentPath
+                ? `${parentPath}/${fileName}`
+                : fileName;
 
-        const oldIsFile = typeof oldFileContents === 'string';
-        const newIsFile = typeof newFileContents === 'string';
-        const existsInOldDir = oldDir[fileName] !== undefined;
-        const existsInNewDir = newDir[fileName] !== undefined;
+            const oldIsFile = typeof oldFileContents === 'string';
+            const newIsFile = typeof newFileContents === 'string';
+            const existsInOldDir = oldDir[fileName] !== undefined;
+            const existsInNewDir = newDir[fileName] !== undefined;
 
-        if (!oldIsFile && !newIsFile) {
-            // This is a directory, we want to recursively call this on it and append the results
-            const subChanges = await diffDirectories({
-                oldDir: oldDir[fileName] as DirectoryContents,
-                newDir: newDir[fileName] as DirectoryContents,
-                parentPath: fullPath,
-            });
-            changes.push(...subChanges);
-            return;
-        }
-
-        if (!existsInOldDir && !existsInNewDir) {
-            throw new Error(
-                `File ${fileName} does not exist in either directory`
-            );
-        }
-
-        if (
-            (oldIsFile && !newIsFile && existsInOldDir && existsInNewDir) ||
-            (newIsFile && !oldIsFile && existsInOldDir && existsInNewDir)
-        ) {
-            throw new Error(
-                `File ${fileName} exists in one directory but not the other`
-            );
-        }
-
-        if (!existsInOldDir && existsInNewDir && newIsFile) {
-            // This is a file that is being added
-            let changeGroup: LineChangeGroup = {
-                startLineNumber: 1,
-                endLineNumber: countLines(newFileContents),
-                changeType: 'add',
-                newContent: newFileContents,
-            };
-            changes.push({
-                fileName: fullPath,
-                lineChangeGroups: [changeGroup],
-                isBinary: isBinaryFile(fileName),
-            });
-            return;
-        }
-
-        if (existsInOldDir && !existsInNewDir && oldIsFile) {
-            // This is a file that is being left in, there is no change required
-            // At first I made the assumption it is a file that is removed, however
-            // upon further thinking - by virtue of not having a file there, it
-            // should be assumed that the file is a main game file or
-            // untouched file in another mod.
-            // For example, if Age of Man did not have a file, but the "oldDir"
-            // (or the base game) did, then of course AOM is not removing it!
-            // So, we can safely skip these changes
-
-            return;
-        }
-
-        if (
-            existsInOldDir &&
-            existsInNewDir &&
-            newIsFile &&
-            oldIsFile &&
-            isBinaryFile(fileName)
-        ) {
-            // This is a binary file that is being changed
-            const changeGroup: LineChangeGroup = {
-                startLineNumber: 1,
-                endLineNumber: countLines(newFileContents),
-                changeType: 'replace',
-                newContent: newFileContents,
-                oldContent: oldFileContents,
-            };
-            changes.push({
-                fileName: fullPath,
-                lineChangeGroups: [changeGroup],
-                isBinary: isBinaryFile(fileName),
-            });
-            console.log(
-                'added binary file to change list as a replace: ',
-                fullPath
-            );
-            return;
-        }
-
-        if (existsInOldDir && existsInNewDir && newIsFile && oldIsFile) {
-            const oldFileHash = hashFileContents(oldFileContents);
-            const newFileHash = hashFileContents(newFileContents);
-
-            if (oldFileHash === newFileHash) {
-                // Files are the same, skip further processing
+            if (!oldIsFile && !newIsFile) {
+                // This is a directory, we want to recursively call this on it and append the results
+                const subChanges = await diffDirectories({
+                    oldDir: oldDir[fileName] as DirectoryContents,
+                    newDir: newDir[fileName] as DirectoryContents,
+                    parentPath: fullPath,
+                });
+                changes.push(...subChanges);
                 return;
             }
 
-            const diffs = await diffTexts(oldFileContents, newFileContents);
-
-            let lineCount = 1;
-
-            for (const diff of diffs) {
-                if (diff.added) {
-                    const changeGroup: LineChangeGroup = {
-                        startLineNumber: lineCount,
-                        endLineNumber: lineCount,
-                        changeType: 'add',
-                        newContent: diff.value,
-                    };
-                    changes.push({
-                        fileName: fullPath,
-                        lineChangeGroups: [changeGroup],
-                        isBinary: isBinaryFile(fileName),
-                    });
-                } else if (diff.removed) {
-                    const changeGroup: LineChangeGroup = {
-                        startLineNumber: lineCount,
-                        endLineNumber: lineCount,
-                        changeType: 'remove',
-                        oldContent: diff.value,
-                    };
-                    changes.push({
-                        fileName: fullPath,
-                        lineChangeGroups: [changeGroup],
-                        isBinary: isBinaryFile(fileName),
-                    });
-                } else {
-                    // Do nothing here, this library handles replace as a remove then an add
-                }
-                if (diff.value.includes('\n')) {
-                    lineCount++;
-                }
+            if (!existsInOldDir && !existsInNewDir) {
+                throw new Error(
+                    `File ${fileName} does not exist in either directory`
+                );
             }
 
-            return;
-        }
-    });
+            if (
+                (oldIsFile && !newIsFile && existsInOldDir && existsInNewDir) ||
+                (newIsFile && !oldIsFile && existsInOldDir && existsInNewDir)
+            ) {
+                throw new Error(
+                    `File ${fileName} exists in one directory but not the other`
+                );
+            }
+
+            if (!existsInOldDir && existsInNewDir && newIsFile) {
+                // This is a file that is being added
+                let changeGroup: LineChangeGroup = {
+                    startLineNumber: 1,
+                    endLineNumber: countLines(newFileContents),
+                    changeType: 'add',
+                    newContent: newFileContents,
+                };
+                changes.push({
+                    fileName: fullPath,
+                    lineChangeGroups: [changeGroup],
+                    isBinary: isBinaryFile(fileName),
+                });
+                return;
+            }
+
+            if (existsInOldDir && !existsInNewDir && oldIsFile) {
+                // This is a file that is being left in, there is no change required
+                // At first I made the assumption it is a file that is removed, however
+                // upon further thinking - by virtue of not having a file there, it
+                // should be assumed that the file is a main game file or
+                // untouched file in another mod.
+                // For example, if Age of Man did not have a file, but the "oldDir"
+                // (or the base game) did, then of course AOM is not removing it!
+                // So, we can safely skip these changes
+
+                return;
+            }
+
+            if (
+                existsInOldDir &&
+                existsInNewDir &&
+                newIsFile &&
+                oldIsFile &&
+                isBinaryFile(fileName)
+            ) {
+                // This is a binary file that is being changed
+                const changeGroup: LineChangeGroup = {
+                    startLineNumber: 1,
+                    endLineNumber: countLines(newFileContents),
+                    changeType: 'replace',
+                    newContent: newFileContents,
+                    oldContent: oldFileContents,
+                };
+                changes.push({
+                    fileName: fullPath,
+                    lineChangeGroups: [changeGroup],
+                    isBinary: isBinaryFile(fileName),
+                });
+                console.log(
+                    'added binary file to change list as a replace: ',
+                    fullPath
+                );
+                return;
+            }
+
+            if (existsInOldDir && existsInNewDir && newIsFile && oldIsFile) {
+                const oldFileHash = hashFileContents(oldFileContents);
+                const newFileHash = hashFileContents(newFileContents);
+
+                if (oldFileHash === newFileHash) {
+                    // Files are the same, skip further processing
+                    return;
+                }
+
+                const diffs = await diffTexts(oldFileContents, newFileContents);
+
+                let lineCount = 1;
+
+                for (const diff of diffs) {
+                    if (diff.added) {
+                        const changeGroup: LineChangeGroup = {
+                            startLineNumber: lineCount,
+                            endLineNumber: lineCount,
+                            changeType: 'add',
+                            newContent: diff.value,
+                        };
+                        changes.push({
+                            fileName: fullPath,
+                            lineChangeGroups: [changeGroup],
+                            isBinary: isBinaryFile(fileName),
+                        });
+                    } else if (diff.removed) {
+                        const changeGroup: LineChangeGroup = {
+                            startLineNumber: lineCount,
+                            endLineNumber: lineCount,
+                            changeType: 'remove',
+                            oldContent: diff.value,
+                        };
+                        changes.push({
+                            fileName: fullPath,
+                            lineChangeGroups: [changeGroup],
+                            isBinary: isBinaryFile(fileName),
+                        });
+                    } else {
+                        // Do nothing here, this library handles replace as a remove then an add
+                    }
+                    if (diff.value.includes('\n')) {
+                        lineCount++;
+                    }
+                }
+
+                return;
+            }
+        })
+    );
 
     await Promise.all(promises);
 

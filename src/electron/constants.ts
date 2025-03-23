7377b96bf376bcef1f31cc839c66040e16d21baf
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-03-22 21:16:51 -0400

    Refactor path handling in constants to use path.join for better cross-platform compatibility

--- src/electron/constants.ts
+++ src/electron/constants.ts
@@ -1,9 +1,14 @@
 import { app } from 'electron';
+import * as path from 'path';
 
 export const DEFAULT_MOD_FOLDER_NAME = 'Mods';
 let DEFAULT_MOD_DIR: string;
 try {
-    DEFAULT_MOD_DIR = `${app.getPath('appData')}\\${app.getName()}\\${DEFAULT_MOD_FOLDER_NAME}`;
+    DEFAULT_MOD_DIR = path.join(
+        app.getPath('appData'),
+        app.getName(),
+        DEFAULT_MOD_FOLDER_NAME
+    );
 } catch (err) {
     // eslint-disable-next-line no-console
     console.error(`An error has occurred getting the appData path: ${err}`);
@@ -12,20 +17,27 @@ try {
 export const DEFAULT_INSTALLS_FOLDER_NAME = 'Installations';
 let DEFAULT_INSTALLS_DIR: string;
 try {
-    DEFAULT_INSTALLS_DIR = `${app.getPath(
-        'appData'
-    )}\\${app.getName()}\\${DEFAULT_INSTALLS_FOLDER_NAME}`;
+    DEFAULT_INSTALLS_DIR = path.join(
+        app.getPath('appData'),
+        app.getName(),
+        DEFAULT_INSTALLS_FOLDER_NAME
+    );
     console.log('default installs dir: ', DEFAULT_INSTALLS_DIR);
 } catch (err) {
     // eslint-disable-next-line no-console
     console.error(`An error has occurred getting the appData path: ${err}`);
 }
 
-export const DEFAULT_INSTALLS_FILE = `${DEFAULT_INSTALLS_DIR}\\installations.json`;
+export const DEFAULT_INSTALLS_FILE = path.join(
+    DEFAULT_INSTALLS_DIR,
+    'installations.json'
+);
 
 export const DEFAULT_BACKUPS_FOLDER_NAME = 'InstallationBackups';
-export const DEFAULT_BACKUPS_FOLDER = `${app.getPath(
-    'appData'
-)}\\${app.getName()}\\${DEFAULT_BACKUPS_FOLDER_NAME}`;
+export const DEFAULT_BACKUPS_FOLDER = path.join(
+    app.getPath('appData'),
+    app.getName(),
+    DEFAULT_BACKUPS_FOLDER_NAME
+);
 
 export { DEFAULT_INSTALLS_DIR, DEFAULT_MOD_DIR };

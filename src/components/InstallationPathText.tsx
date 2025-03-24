bb31a0c809fa690bbec88f4eb58e981e33a669f3
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-03-24 07:32:17 -0400

    Refactor InstallationPathText component to use ReadonlyDeep for props type safety

--- src/components/InstallationPathText.tsx
+++ src/components/InstallationPathText.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
+import { ReadonlyDeep } from 'type-fest';
 
 type Props = {
     dir: string;
     installationType?: 'gog' | 'steam';
 };
 
-export const InstallationPathText: FC<Props> = (props: Props) => (
+export const InstallationPathText: FC<Props> = (props: ReadonlyDeep<Props>) => (
     <div className="flex items-center gap-2">
         {props.installationType && (
             <span className="badge badge-outline font-semibold">

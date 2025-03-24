b773e0eeb4e44c95531ff7bc8df0b0b4f466efc7
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-03-24 07:49:48 -0400

    Remove unused cookie handling and related code from Settings component

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,16 +1,5 @@
-import Cookie from 'js-cookie';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { themeChange } from 'theme-change';
-
-// TODO finish this after configuring the proper locations within the Electron-side
-
-const MOD_STORAGE_DIR_COOKIE_NAME = 'modStorageDirectory';
-const INITIAL_MOD_STORAGE_DIR = ''; // TODO eventually replace this with Linux and Mac as well
-
-const initialModStorageCookie =
-    Cookie.get(MOD_STORAGE_DIR_COOKIE_NAME) === undefined
-        ? ''
-        : Cookie.get(MOD_STORAGE_DIR_COOKIE_NAME);
 
 export const Settings: FC = (): React.ReactElement => {
     useEffect(() => {

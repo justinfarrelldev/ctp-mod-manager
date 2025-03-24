06aa976d536ed474c792bb218c031138bab7814d
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-03-24 10:24:10 -0400

    Refactor isValidInstall to use a constant for the checked directory name

--- src/electron/file/isValidInstall.ts
+++ src/electron/file/isValidInstall.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
 
+const CTP2_CHECKED_DIR = 'ctp2_data';
+
 export const isValidInstall = async (dir: string): Promise<boolean> => {
     // If there is a ctp2_data dir on the top level, it is a valid install
     return (
-        fs.readdirSync(dir).filter((file) => file.endsWith('ctp2_data'))
+        fs.readdirSync(dir).filter((file) => file.endsWith(CTP2_CHECKED_DIR))
             .length > 0
     );
 };
